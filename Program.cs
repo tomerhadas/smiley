9b7977132e5ebda03022d6285aa212140731fb39
tomerhadas <thadas112@gmail.com>
2025-01-31 14:25:28 +0200

    Add shocked smiley

--- Program.cs
+++ Program.cs
@@ -10,5 +10,6 @@ internal class Program
         Smiley.Sad();
         Smiley.Bored();
         Smiley.Crazy();
+        Smiley.Shocked();
     }
 }
--- Smiley.cs
+++ Smiley.cs
@@ -24,4 +24,9 @@ internal static class Smiley
     {
         Console.WriteLine(":-*");
     }
+    public static void Shocked()
+    {
+        Console.WriteLine(":-O");
+    }
+
 }
